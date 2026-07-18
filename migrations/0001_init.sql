f1bb6acdca27c68ae61db3e0c043d22880271cde
Roscpy <129000487+Roscpy@users.noreply.github.com>
2026-07-17 22:31:54 -0400

    Create 0001_init.sql

---
+++ migrations/0001_init.sql
@@ -0,0 +1,6 @@
+# migrations/0001_init.sql
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
